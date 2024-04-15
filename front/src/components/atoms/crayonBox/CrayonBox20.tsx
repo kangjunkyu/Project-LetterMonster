9d304f3d1702c7c36e242fb9930cb061fc3866da
이호진 <skwo777@naver.com>
2024-04-15 13:18:15 +0900

    Design: 정적, 동적 박스 분할
    #S10P31B103-73

--- front/src/components/atoms/crayonBox/CrayonBox20.tsx
+++ front/src/components/atoms/crayonBox/CrayonBox20.tsx
@@ -1,12 +1,36 @@
-import { ReactNode } from "react";
+import { useState, useEffect } from "react";
 import "./CrayonBox20.scss";
 
 interface Props {
-  children: ReactNode;
+  children: React.ReactNode;
 }
 
 function CrayonBox20({ children }: Props) {
-  return <section className="BoxContainer">{children}</section>;
+  const [currentClass, setCurrentClass] = useState("BoxContainer");
+  const classes = [
+    "BoxContainer",
+    "BoxContainer2",
+    "BoxContainer3",
+    "BoxContainer4",
+  ];
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentClass((current) => {
+        // 현재 클래스 인덱스 계산
+        const currentIndex = classes.indexOf(current);
+        // 다음 클래스 인덱스 계산
+        const nextIndex = (currentIndex + 1) % classes.length;
+        // 다음 클래스 반환
+        return classes[nextIndex];
+      });
+    }, 1000);
+
+    // 컴포넌트 언마운트 시 인터벌 정리
+    return () => clearInterval(intervalId);
+  }, []); // 빈 의존성 배열을 사용하여 마운트 시 한 번만 설정
+
+  return <section className={currentClass}>{children}</section>;
 }
 
 export default CrayonBox20;
