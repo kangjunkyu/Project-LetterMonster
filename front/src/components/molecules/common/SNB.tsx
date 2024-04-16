946a8bb76b3d40833d5555d5a6e5efa4c9adf698
이호진 <skwo777@naver.com>
2024-04-16 17:14:47 +0900

    Refactor: 파일 이동
    #S10P31B103-67

--- front/src/components/molecules/common/SNB.tsx
+++ front/src/components/molecules/common/SNB.tsx
@@ -1,20 +1,26 @@
 import { Link } from "react-router-dom";
+
 import { Page_Url } from "../../../router/Page_Url";
 import styles from "./SNB.module.scss";
+
+//아이콘
+import red from "../../../assets/GNBIcon/red.png";
+import yellow from "../../../assets/GNBIcon/yellow.png";
+import green from "../../../assets/GNBIcon/green.png";
 
 function SNB() {
   return (
     <nav>
       <Link className={styles.SNBButton} to={Page_Url.Main}>
-        <img src="/red.png" alt="그리기" />
+        <img src={red} alt="그리기" />
         그리기
       </Link>
       <Link className={styles.SNBButton} to={Page_Url.Main}>
-        <img src="/yellow.png" alt="스케치북" />
+        <img src={yellow} alt="스케치북" />
         스케치북
       </Link>
       <Link className={styles.SNBButton} to={Page_Url.Main}>
-        <img src="/green.png" alt="더보기" />
+        <img src={green} alt="더보기" />
         더보기
       </Link>
     </nav>
