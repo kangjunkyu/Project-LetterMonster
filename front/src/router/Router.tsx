01d490aa24491bfcba8aa534e3cfc80ed96b1ba8
이호진 <skwo777@naver.com>
2024-04-16 16:24:54 +0900

    Refactor: 라우터 수정
    #S10P31B103-67

--- front/src/router/Router.tsx
+++ front/src/router/Router.tsx
@@ -1,19 +1,26 @@
 import { Route, Routes } from "react-router-dom";
 import { Page_Url } from "./Page_Url";
 // 레이아웃
+import DefaultLayouts from "../styles/layouts/DefaultLayouts";
 
 // 페이지
 import MainPage from "../components/pages/main/MainPage";
+import ErrorPage from "../components/pages/error/ErrorPage";
 import LoginPage from "../components/pages/login/LoginPage";
 import KakaoCallback from "../components/pages/login/KakaoCallback";
 
 function Router() {
   return (
     <Routes>
-      <Route path={Page_Url.Main} element={<MainPage />}></Route>
-      <Route path={Page_Url.Login} element={<LoginPage />}></Route>
+      <Route element={<DefaultLayouts />} errorElement={<ErrorPage />}>
+        <Route path={Page_Url.Main} element={<MainPage />}></Route>
 
-      <Route path={Page_Url.KakaoCallback} element={<KakaoCallback />}></Route>
+        <Route path={Page_Url.Login} element={<LoginPage />}></Route>
+        <Route
+          path={Page_Url.KakaoCallback}
+          element={<KakaoCallback />}
+        ></Route>
+      </Route>
     </Routes>
   );
 }
