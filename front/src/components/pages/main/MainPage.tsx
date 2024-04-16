807af185ec9a3f7560b06cf0088646a2cf85a979
이호진 <skwo777@naver.com>
2024-04-16 16:30:25 +0900

    Refactor: css module 적용
    #S10P31B103-67

--- front/src/components/pages/main/MainPage.tsx
+++ front/src/components/pages/main/MainPage.tsx
@@ -1,25 +1,57 @@
 import { useNavigate } from "react-router-dom";
 import { Page_Url } from "../../../router/Page_Url";
-import "./MainPage.scss";
+import styles from "./MainPage.module.scss";
 
 function MainPage() {
   const navigate = useNavigate();
 
   return (
-    <main className="container">
+    <div className={styles.container}>
       <div>Letter Monster / 레터몬스터</div>
-      <div className="characterDiv">
-        <img src="/src/assets/characterSample/gom.gif" alt="" />
-        <img src="/src/assets/characterSample/hojin_character.gif" alt="" />
-        <img src="/src/assets/characterSample/egypt.gif" alt="" />
-        <img src="/src/assets/characterSample/rabbit.gif" alt="" />
-        <img src="/src/assets/characterSample/shinzzang.gif" alt="" />
-        <img src="/src/assets/characterSample/television.gif" alt="" />
-        <img src="/src/assets/characterSample/attamoma.gif" alt="" />
-        <img src="/src/assets/characterSample/juhyeon.gif" alt="" />
+      <div className={styles.characterDiv}>
+        <img
+          className={styles.character}
+          src="/src/assets/characterSample/gom.gif"
+          alt=""
+        />
+        <img
+          className={styles.character}
+          src="/src/assets/characterSample/hojin_character.gif"
+          alt=""
+        />
+        <img
+          className={styles.character}
+          src="/src/assets/characterSample/egypt.gif"
+          alt=""
+        />
+        <img
+          className={styles.character}
+          src="/src/assets/characterSample/rabbit.gif"
+          alt=""
+        />
+        <img
+          className={styles.character}
+          src="/src/assets/characterSample/shinzzang.gif"
+          alt=""
+        />
+        <img
+          className={styles.character}
+          src="/src/assets/characterSample/television.gif"
+          alt=""
+        />
+        <img
+          className={styles.character}
+          src="/src/assets/characterSample/attamoma.gif"
+          alt=""
+        />
+        <img
+          className={styles.character}
+          src="/src/assets/characterSample/juhyeon.gif"
+          alt=""
+        />
       </div>
       <button onClick={() => navigate(Page_Url.Login)}>카카오로그인</button>
-    </main>
+    </div>
   );
 }
 
