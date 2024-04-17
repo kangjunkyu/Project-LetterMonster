e83c3fbe4e5e5bd2d3f573b181183bd0a1f93c84
이호진 <skwo777@naver.com>
2024-04-17 14:42:56 +0900

    Refactor: 편지 작성 메뉴 추가
    #S10P31B103-74

--- front/src/components/molecules/common/SNB.tsx
+++ front/src/components/molecules/common/SNB.tsx
@@ -7,11 +7,12 @@ import styles from "./SNB.module.scss";
 import red from "../../../assets/GNBIcon/red.png";
 import yellow from "../../../assets/GNBIcon/yellow.png";
 import green from "../../../assets/GNBIcon/green.png";
+import blue from "../../../assets/GNBIcon/blue.png";
 
 function SNB() {
   return (
-    <nav>
-      <Link className={styles.SNBButton} to={Page_Url.Main}>
+    <nav className={styles.SNBNav}>
+      <Link className={styles.SNBButton} to={Page_Url.WriteLetter}>
         <img src={red} alt="그리기" />
         그리기
       </Link>
@@ -20,7 +21,11 @@ function SNB() {
         스케치북
       </Link>
       <Link className={styles.SNBButton} to={Page_Url.Main}>
-        <img src={green} alt="더보기" />
+        <img src={green} alt="편지 쓰기" />
+        편지 쓰기
+      </Link>
+      <Link className={styles.SNBButton} to={Page_Url.Main}>
+        <img src={blue} alt="더보기" />
         더보기
       </Link>
     </nav>
