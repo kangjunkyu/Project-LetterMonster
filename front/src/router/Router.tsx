265126d5f4221284742e75d290f05c6e3086d504
이호진 <skwo777@naver.com>
2024-04-18 02:15:06 +0900

    Refactor: 라인 로그인 라우팅 추가

--- front/src/router/Router.tsx
+++ front/src/router/Router.tsx
@@ -8,6 +8,7 @@ import MainPage from "../components/pages/main/MainPage";
 import ErrorPage from "../components/pages/error/ErrorPage";
 import LoginPage from "../components/pages/login/LoginPage";
 import KakaoCallback from "../components/pages/login/KakaoCallback";
+import LineCallback from "../components/pages/login/LineCallback";
 import SketchPage from "../components/pages/sketch/SketchPage";
 import MotionPage from "../components/pages/motion/MotionPage";
 import LetterWritePage from "../components/pages/letter/WriteLetterPage";
@@ -16,10 +17,10 @@ function Router() {
   return (
     <Routes>
       <Route element={<DefaultLayouts />} errorElement={<ErrorPage />}>
-      <Route path={Page_Url.Main} element={<MainPage />}></Route>
-      <Route path={Page_Url.Login} element={<LoginPage />}></Route>
-      <Route path={Page_Url.Sketch} element={<SketchPage />}></Route>
-      <Route path={Page_Url.Motion} element={<MotionPage />}></Route>
+        <Route path={Page_Url.Main} element={<MainPage />}></Route>
+        <Route path={Page_Url.Login} element={<LoginPage />}></Route>
+        <Route path={Page_Url.Sketch} element={<SketchPage />}></Route>
+        <Route path={Page_Url.Motion} element={<MotionPage />}></Route>
 
         <Route path={Page_Url.Login} element={<LoginPage />}></Route>
         <Route
@@ -30,6 +31,7 @@ function Router() {
           path={Page_Url.KakaoCallback}
           element={<KakaoCallback />}
         ></Route>
+        <Route path={Page_Url.LineCallback} element={<LineCallback />}></Route>
       </Route>
     </Routes>
   );
