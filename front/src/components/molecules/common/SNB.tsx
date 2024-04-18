fa33c7773a6e8a4e4b259406be37c17c71283189
이호진 <skwo777@naver.com>
2024-04-18 09:51:54 +0900

    Refactor: SEO 최적화
    #S10P31B103-67

--- front/src/components/molecules/common/SNB.tsx
+++ front/src/components/molecules/common/SNB.tsx
@@ -13,19 +13,19 @@ function SNB() {
   return (
     <nav className={styles.SNBNav}>
       <Link className={styles.SNBButton} to={Page_Url.WriteLetter}>
-        <img src={red} alt="그리기" />
+        <img src={red} alt="빨강 크레파스" />
         그리기
       </Link>
       <Link className={styles.SNBButton} to={Page_Url.Main}>
-        <img src={yellow} alt="스케치북" />
+        <img src={yellow} alt="노랑 크레파스" />
         스케치북
       </Link>
       <Link className={styles.SNBButton} to={Page_Url.Main}>
-        <img src={green} alt="편지 쓰기" />
+        <img src={green} alt="초록 크레파스" />
         편지 쓰기
       </Link>
       <Link className={styles.SNBButton} to={Page_Url.Main}>
-        <img src={blue} alt="더보기" />
+        <img src={blue} alt="파랑 크레파스" />
         더보기
       </Link>
     </nav>
