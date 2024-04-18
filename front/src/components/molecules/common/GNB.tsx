f19e956781a1b0a658e8d74e7fcdab98593f5556
이호진 <skwo777@naver.com>
2024-04-18 23:18:28 +0900

    Design: 메뉴 언어 전환 추가

--- front/src/components/molecules/common/GNB.tsx
+++ front/src/components/molecules/common/GNB.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import styles from "./GNB.module.scss";
+import { useTranslation } from "react-i18next";
+
 import { Page_Url } from "../../../router/Page_Url";
 
 // 아이콘
@@ -8,23 +10,24 @@ import draw from "../../../assets/GNBIcon/draw.svg";
 import more from "../../../assets/GNBIcon/more.svg";
 
 function GNB() {
+  const { t } = useTranslation();
   return (
     <header className={styles.header}>
-      <Link className={styles.GNBbutton} to={Page_Url.Main}>
+      <Link className={styles.GNBbutton} to={Page_Url.Sketch}>
         <img src={draw} alt="그리기" />
-        캐릭터그리기
+        {t("nav.drawing")}
       </Link>
       <Link className={styles.GNBbutton} to={Page_Url.Main}>
         <img src={sketchbook} alt="스케치북" />
-        스케치북
+        {t("nav.sketchbook")}
       </Link>
-      <Link className={styles.GNBbutton} to={Page_Url.Main}>
+      <Link className={styles.GNBbutton} to={Page_Url.WriteLetter}>
         <img src={sketchbook} alt="편지 쓰기" />
-        편지쓰기
+        {t("nav.letter")}
       </Link>
       <Link className={styles.GNBbutton} to={Page_Url.Main}>
         <img src={more} alt="메인" />
-        더보기
+        {t("nav.more")}
       </Link>
     </header>
   );
