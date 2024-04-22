73eacbe431ba1a368fe06b2eddc883f85b7959e3
손주현 <juhyeon042@naver.com>
2024-04-22 17:43:25 +0900

    Refactor: 주석 해제

--- Lemon/backend/src/main/java/com/lemon/backend/global/config/FilterConfig.java
+++ Lemon/backend/src/main/java/com/lemon/backend/global/config/FilterConfig.java
@@ -1,6 +1,5 @@
 package com.lemon.backend.global.config;
 
-import com.lemon.backend.domain.users.user.repository.UserRepository;
 import com.lemon.backend.global.filter.CorsFilter;
 import com.lemon.backend.global.filter.JwtAuthenticationFilter;
 import com.lemon.backend.global.jwt.JwtTokenProvider;
@@ -11,21 +10,21 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FilterConfig {
-//    @Bean
-//    public FilterRegistrationBean corsFilter() {
-//        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
-//        filterRegistrationBean.setFilter(new CorsFilter());
-//        filterRegistrationBean.setOrder(1);
-//        return filterRegistrationBean;
-//    }
-//
-//    @Bean
-//    public FilterRegistrationBean<JwtAuthenticationFilter> jwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
-//        FilterRegistrationBean<JwtAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
-//        registrationBean.setFilter(new JwtAuthenticationFilter(jwtTokenProvider));
-//        registrationBean.setOrder(2);
-//        registrationBean.addUrlPatterns("/*");
-//        return registrationBean;
-//    }
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new CorsFilter());
+        filterRegistrationBean.setOrder(1);
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<JwtAuthenticationFilter> jwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        FilterRegistrationBean<JwtAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new JwtAuthenticationFilter(jwtTokenProvider));
+        registrationBean.setOrder(2);
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
+    }
 
 }
