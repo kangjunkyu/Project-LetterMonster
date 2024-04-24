5f3780791808a061b6d9dd850c2e4f10f51645b2
구본민 <gugabm@naver.com>
2024-04-24 16:35:56 +0900

    Feat: my page 설계
    #S10P31B103-123

---
+++ front/src/components/pages/mypage/MyPages.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { useLogout } from "../../../hooks/auth/useLogout";
+import { usePostNickname } from "../../../hooks/user/usePostNickname";
+import styles from "./MyPage.module.scss";
+import { useGetUserNickname } from "../../../hooks/user/useGetUserNickName";
+import { useDeleteUser } from "../../../hooks/user/useDeleteUser";
+import MyPageUserInfo from "../../molecules/mypage/MyPageUserInfo";
+
+function MyPages() {
+  const logout = useLogout();
+  const deleteUser = useDeleteUser();
+  const changeNickname = usePostNickname();
+  const [nickname, setNickname] = useState("");
+  const { data: userInfo } = useGetUserNickname();
+
+  const postNicknameMutation = () => changeNickname.mutate(nickname);
+
+  return (
+    <>
+      <div className={styles.myPageContainer}>
+        <MyPageUserInfo/>
+        <div>
+          <button onClick={() => logout()}>로그아웃</button>
+        </div>
+
+
+
+        
+        <div>
+          <button onClick={() => deleteUser()}>회원탈퇴</button>
+        </div>
+        <div>
+          {userInfo && <div>현재 닉네임 : {userInfo.nickname}</div>}
+          {userInfo && <div>현재 닉네임태그 : {userInfo.nicknameTag}</div>}
+          <input
+            type="text"
+            onChange={(e) => {
+              setNickname(e.target.value);
+            }}
+          />
+          <button onClick={() => postNicknameMutation()}>닉네임변경</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MyPages;
