dc07f4d9bf400ffa0e4b9938b2d85d51c7c0aaaf
구본민 <gugabm@naver.com>
2024-04-25 22:12:36 +0900

    Feat: motion gif 생성 구현
    #S10P31B103-127

--- front/src/components/pages/motion/MotionPage.tsx
+++ front/src/components/pages/motion/MotionPage.tsx
@@ -1,19 +1,31 @@
-// import { useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import styles from "./MotionPage.module.scss";
 import MotionList from "../../molecules/motion/MotionList";
 import MotionPreview from "../../molecules/motion/MotionPreview";
+import { useState } from "react";
 
 function MotionPage() {
-  // const location = useLocation();
-  // const { image } = location.state || {};
+  const location = useLocation();
+  const { characterId, nickname } = location.state || {};
+  const [gif, setGif] = useState("");
+  const [motionId, setMotionId] = useState(0);
 
   return (
     <div className={styles.motionContainer}>
       <div>
-        <MotionPreview/>
+        <MotionPreview
+          gif={gif}
+          characterNickname={nickname}
+          characterId={characterId}
+          motionId={motionId}
+        />
       </div>
       <div>
-        <MotionList/>
+        <MotionList
+          setGif={setGif}
+          setMotionId={setMotionId}
+          characterId={characterId}
+        />
       </div>
     </div>
   );
