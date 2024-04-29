a62fef70669ccc4ef202352aa366b0838db815b8
강준규 <wnsrb933@naver.com>
2024-04-29 17:48:39 +0900

    Feat: Firebase 초기 설정 및 의존성 추가

---
+++ back/src/main/java/com/lemon/backend/global/config/FCMConfig.java
@@ -0,0 +1,39 @@
+package com.lemon.backend.global.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import io.jsonwebtoken.io.IOException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class FCMConfig {
+
+    @Bean
+    FirebaseMessaging firebaseMessaging() throws IOException, java.io.IOException {
+        ClassPathResource resource = new ClassPathResource("firebase/lemon-8e34d-firebase-adminsdk-vjl60-ea10a56333.json");
+
+        InputStream inputStream = resource.getInputStream();
+
+        FirebaseApp firebaseApp = null;
+        List<FirebaseApp> firebaseApps = FirebaseApp.getApps();
+
+        if(firebaseApps != null && !firebaseApps.isEmpty()){
+            for (FirebaseApp app : firebaseApps) {
+                if(app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)){
+                    firebaseApp = app;
+                }
+            }
+        }else {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(inputStream))
+                    .build();
+            firebaseApp = FirebaseApp.initializeApp(options);
+        }
+        return FirebaseMessaging.getInstance(firebaseApp);
+    }
+}
