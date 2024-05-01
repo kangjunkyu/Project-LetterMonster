085119201cc651247dfc15725d6bad76950dede5
HJ-dp <skwo777@hufs.ac.kr>
2024-05-01 22:40:58 +0900

    Chore: 임시 버튼 생성

--- front/src/components/pages/welcome/WelcomePage.tsx
+++ front/src/components/pages/welcome/WelcomePage.tsx
@@ -1,52 +1,13 @@
+import { Link } from "react-router-dom";
 import styles from "./WelcomePage.module.scss";
+import { Page_Url } from "../../../router/Page_Url";
 
 function WelcomePage() {
   return (
     <div className={styles.WelcomeContainer}>
       <div>{`레터 몬스터는 <24/04/28>일 1차 배포 목표로 준비중입니다.`}</div>
       <div>{`レターモンスターは <24/04/28> 臨時配布を目指して頑張ってます。ლ(•̀ _ •́ ლ)`}</div>
-      <div>
-        <img
-          className={styles.character}
-          src="/src/assets/characterSample/gom.gif"
-          alt=""
-        />
-        <img
-          className={styles.character}
-          src="/src/assets/characterSample/hojin_character.gif"
-          alt=""
-        />
-        <img
-          className={styles.character}
-          src="/src/assets/characterSample/egypt.gif"
-          alt=""
-        />
-        <img
-          className={styles.character}
-          src="/src/assets/characterSample/rabbit.gif"
-          alt=""
-        />
-        <img
-          className={styles.character}
-          src="/src/assets/characterSample/shinzzang.gif"
-          alt=""
-        />
-        <img
-          className={styles.character}
-          src="/src/assets/characterSample/television.gif"
-          alt=""
-        />
-        <img
-          className={styles.character}
-          src="/src/assets/characterSample/attamoma.gif"
-          alt=""
-        />
-        <img
-          className={styles.character}
-          src="/src/assets/characterSample/juhyeon.gif"
-          alt=""
-        />
-      </div>
+      <Link to={Page_Url.Main}>임시</Link>
     </div>
   );
 }
