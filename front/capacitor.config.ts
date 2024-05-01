47dd3b7dfe9a3244c4947aaa59c5024874e6fcdc
구본민 <gugabm@naver.com>
2024-05-01 17:05:12 +0900

    Feat: capacitor, android, ios 초기설정
    #S10P31B103-194

---
+++ front/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.project.lettermon',
+  appName: 'LetterMon',
+  webDir: 'build'
+};
+
+export default config;
