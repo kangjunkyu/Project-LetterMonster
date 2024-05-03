57571d5dbe11ada682e22f32e9e8c7c3d6e47fb8
조연주 <yjyk9679@naver.com>
2024-05-03 23:52:26 +0900

    Fix: 경로 ERROR등 수정

--- ai-server/AnimatedDrawings/examples/image_to_animation.py
+++ ai-server/AnimatedDrawings/examples/image_to_animation.py
@@ -2,8 +2,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .image_to_annotations import image_to_annotations
-from .annotations_to_animation import annotations_to_animation
+from AnimatedDrawings.examples.image_to_annotations import image_to_annotations
+from AnimatedDrawings.examples.annotations_to_animation import annotations_to_animation
 from pathlib import Path
 import logging
 import sys
