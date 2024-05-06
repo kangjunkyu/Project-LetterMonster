2ac942f945fe982113b6f1a10488b17f95c8795a
강준규 <wnsrb933@naver.com>
2024-05-06 22:34:57 +0900

    Feat: Gruop test code

--- back/src/test/java/com/lemon/backend/domain/friend/GroupServiceTest.java
+++ back/src/test/java/com/lemon/backend/domain/friend/GroupServiceTest.java
@@ -18,9 +18,10 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
-class GroupsServiceTest {
+class GroupServiceTest {
 
     @Mock
     private GroupsRepository groupRepository;
@@ -36,32 +37,58 @@ class GroupsServiceTest {
     void setup() {
         MockitoAnnotations.initMocks(this);
     }
-//
-//    @Test
-//    void createGroup_Success() {
-//        Users user = new Users();
-//        user.setId(1);
-//        when(userRepository.findById(1)).thenReturn(Optional.of(user));
-//        when(groupRepository.save(any(GroupsInfo.class))).thenReturn(new GroupsInfo());
-//
-//        assertDoesNotThrow(() -> groupsService.createGroup(1, "New Group"));
-//        verify(groupRepository).save(any(GroupsInfo.class));
-//    }
-//
-//    @Test
-//    void deleteGroup_Success() {
-//        GroupsInfo group = new GroupsInfo();
-//        when(groupRepository.findById(1L)).thenReturn(Optional.of(group));
-//        when(groupRepository.findFirstByUsersId(1)).thenReturn(Optional.of(new GroupsInfo()));
-//
-//        assertDoesNotThrow(() -> groupsService.deleteGroup(1, 1L));
-//        verify(groupRepository).deleteById(1L);
-//    }
-//
-//    @Test
-//    void deleteGroup_NotFound() {
-//        when(groupRepository.findById(1L)).thenReturn(Optional.empty());
-//
-//        assertThrows(CustomException.class, () -> groupsService.deleteGroup(1, 1L));
-//    }
+
+    @Test
+    void createGroup_Success() {
+        Users user = Users.builder()
+                .id(1)
+                .build();
+        GroupsInfo groupsInfo = GroupsInfo.builder()
+                .id(1L)
+                .groupName("test")
+                .owner(user)
+                .build();
+        when(userRepository.findById(1)).thenReturn(Optional.of(user));
+        when(groupRepository.save(any(GroupsInfo.class))).thenReturn(groupsInfo);
+
+        assertDoesNotThrow(() -> groupsService.createGroup(1, "New Group"));
+        verify(groupRepository).save(any(GroupsInfo.class));
+    }
+
+    @Test
+    void deleteGroup_Success() {
+        Users user = Users.builder()
+                .id(1)
+                .build();
+        Users friend = Users.builder()
+                .id(2)
+                .build();
+
+        GroupsInfo groupsInfo = GroupsInfo.builder()
+                .id(1L)
+                .groupName("test")
+                .owner(user)
+                .friendList(new ArrayList<>())
+                .build();
+
+        Friends friends = Friends.builder()
+                .id(1L)
+                .friend(friend)
+                .users(user)
+                .groupsInfo(groupsInfo)
+                .build();
+        groupsInfo.getFriendList().add(friends);
+        when(groupRepository.findById(1L)).thenReturn(Optional.of(groupsInfo));
+        when(groupRepository.findFirstByUsersId(1)).thenReturn(Optional.of(groupsInfo));
+
+        assertDoesNotThrow(() -> groupsService.deleteGroup(1, 1L));
+        verify(groupRepository).deleteById(1L);
+    }
+
+    @Test
+    void deleteGroup_NotFound() {
+        when(groupRepository.findById(1L)).thenReturn(Optional.empty());
+
+        assertThrows(CustomException.class, () -> groupsService.deleteGroup(1, 1L));
+    }
 }
