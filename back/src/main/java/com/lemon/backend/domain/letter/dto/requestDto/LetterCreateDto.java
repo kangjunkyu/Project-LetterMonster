505fabec2c8d1e8388a66c442d15aaa37d38cc4c
강준규 <wnsrb933@naver.com>
2024-05-07 16:03:14 +0900

    Refactor: 친구 추가시 알림 추가

--- back/src/main/java/com/lemon/backend/domain/letter/dto/requestDto/LetterCreateDto.java
+++ back/src/main/java/com/lemon/backend/domain/letter/dto/requestDto/LetterCreateDto.java
@@ -15,9 +15,9 @@ public class LetterCreateDto {
 
     //    private Integer sender;
 //    private Integer receiver;
-    @NotBlank(message = "편지 내용은 공백, 띄어쓰기로 만들 수 없습니다.")
 //    @Pattern(regexp = "^[^<>'\"&;#　]*$", message = "아래 특수 문자는 사용할 수 없습니다. <, >, ', \", &, ;, #, 　")
     private Long id;
+    @NotBlank(message = "편지 내용은 공백, 띄어쓰기로 만들 수 없습니다.")
     private String content;
     private Long characterMotionId;
     private Long sketchbookId;
