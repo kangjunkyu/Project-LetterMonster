dbeda01a58682d288539e68c9350cd5bc3449ed9
이호진 <skwo777@naver.com>
2024-05-10 15:01:48 +0900

    Refactor: 모션 결과 CSS 수정

--- front/src/components/pages/motion/MotionResultPage.tsx
+++ front/src/components/pages/motion/MotionResultPage.tsx
@@ -8,30 +8,28 @@ function MotionResultPage() {
   const navigate = useNavigate();
   const { gif, characterId, nickname, motionId } = location.state || {};
   return (
-    <>
-      <div className={styles.motionResultContainer}>
-        <div>모션 결과 페이지</div>
-        <img
-          className={styles.motionResultImage}
-          src={gif}
-          alt="Gif Result Preview"
-        />
-        <DefaultButton
-          onClick={() => {
-            navigate(Page_Url.WriteLetter, {
-              state: {
-                gif: gif.imageUrl,
-                characterId: characterId,
-                nickname: nickname,
-                motionId: motionId,
-              },
-            });
-          }}
-        >
-          편지 쓰기
-        </DefaultButton>
-      </div>
-    </>
+    <div className={styles.motionResultContainer}>
+      <h1>모션 결과 페이지</h1>
+      <img
+        className={styles.motionResultImage}
+        src={gif}
+        alt="Gif Result Preview"
+      />
+      <DefaultButton
+        onClick={() => {
+          navigate(Page_Url.WriteLetter, {
+            state: {
+              gif: gif.imageUrl,
+              characterId: characterId,
+              nickname: nickname,
+              motionId: motionId,
+            },
+          });
+        }}
+      >
+        편지 쓰기
+      </DefaultButton>
+    </div>
   );
 }
 
