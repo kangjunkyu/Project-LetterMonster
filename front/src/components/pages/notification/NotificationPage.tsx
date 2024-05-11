0ae99771e8b6d1949997752f77e32743ed83e6f5
이호진 <skwo777@naver.com>
2024-05-12 00:33:14 +0900

    Feat: 알림 페이지 퍼블리싱
    #S10P31B103-232

--- front/src/components/pages/notification/NotificationPage.tsx
+++ front/src/components/pages/notification/NotificationPage.tsx
@@ -1,16 +1,23 @@
 import useNotification, {
   useUncheckedNotification,
 } from "../../../hooks/notification/useNotification";
-// import styles from "./NotificationPage.module.scss";
+import NotificationList from "../../molecules/notification/NotificationList";
+import styles from "./NotificationPage.module.scss";
 
 function NotificationPage() {
   const { data } = useNotification();
   const { data: unchecked } = useUncheckedNotification();
   return (
-    <>
-      <div>{data && <div>{data}</div>}</div>
-      <div>{unchecked && <div>{unchecked}</div>}</div>
-    </>
+    <article className={styles.notificationPageContainer}>
+      <section>
+        <h3>새로운 알림</h3>
+        <NotificationList List={data} />
+      </section>
+      <section>
+        <h3>알림 목록</h3>
+        <NotificationList List={unchecked} />
+      </section>
+    </article>
   );
 }
 
