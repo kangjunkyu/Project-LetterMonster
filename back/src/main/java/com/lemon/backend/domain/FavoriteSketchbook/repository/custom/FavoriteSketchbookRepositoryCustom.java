1e02c0eb7f69e4c5c1ec976dcfbca338eb6dd652
강준규 <wnsrb933@naver.com>
2024-05-13 12:16:39 +0900

    Refactor: 즐겨찾기 로직 수정

--- back/src/main/java/com/lemon/backend/domain/FavoriteSketchbook/repository/custom/FavoriteSketchbookRepositoryCustom.java
+++ back/src/main/java/com/lemon/backend/domain/FavoriteSketchbook/repository/custom/FavoriteSketchbookRepositoryCustom.java
@@ -1,6 +1,7 @@
 package com.lemon.backend.domain.FavoriteSketchbook.repository.custom;
 
 import com.lemon.backend.domain.FavoriteSketchbook.dto.FavoriteSketchbookGetDto;
+import com.lemon.backend.domain.FavoriteSketchbook.entity.FavoriteSketchbook;
 import com.lemon.backend.domain.sketchbook.dto.responseDto.SketchbookGetFromFavoriteDto;
 
 import java.util.List;
@@ -10,6 +11,8 @@ public interface FavoriteSketchbookRepositoryCustom {
     Optional<List<FavoriteSketchbookGetDto>> findByUserId(Integer userId);
 
     List<SketchbookGetFromFavoriteDto> findByUserId2(Integer userId);
+
+    FavoriteSketchbook findFavoriteSketchbook(Long sketchbookId);
 
     Boolean checkFavoriteSketchbook(Integer userId, Long sketchbookId);
 }
