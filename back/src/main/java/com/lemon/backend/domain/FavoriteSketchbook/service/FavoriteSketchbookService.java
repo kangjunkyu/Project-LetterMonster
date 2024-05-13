23b7c30f53812df20711b78f4baf23fd0ef90b64
강준규 <wnsrb933@naver.com>
2024-05-13 12:39:51 +0900

    Refactor: 즐겨칮기 기능 로직 변경(action 삭제)

--- back/src/main/java/com/lemon/backend/domain/FavoriteSketchbook/service/FavoriteSketchbookService.java
+++ back/src/main/java/com/lemon/backend/domain/FavoriteSketchbook/service/FavoriteSketchbookService.java
@@ -3,6 +3,7 @@ package com.lemon.backend.domain.FavoriteSketchbook.service;
 import com.lemon.backend.domain.FavoriteSketchbook.dto.FavoriteSketchbookGetDto;
 import com.lemon.backend.domain.FavoriteSketchbook.entity.FavoriteSketchbook;
 import com.lemon.backend.domain.sketchbook.dto.responseDto.SketchbookGetFromFavoriteDto;
+import jakarta.transaction.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +12,9 @@ public interface FavoriteSketchbookService {
     String addFavoriteSketchbook(Integer userId, Long sketchbookId);
 
     void deleteFavotieSketchbook(Integer userId, Long sketchbookId);
+
+    @Transactional
+    String toggleFavoriteSketchbook(Integer userId, Long sketchbookId);
 
     Optional<List<FavoriteSketchbookGetDto>> getFavoriteSketchbooksByUser(Integer userId);
 
