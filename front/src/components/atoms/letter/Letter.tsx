5bb5adac62f9f5fef20f7dc90b203778fdaff334
이호진 <skwo777@naver.com>
2024-05-16 14:04:57 +0900

    Feat: 편지 수신 시간 표시
    #S10P31B103-298

--- front/src/components/atoms/letter/Letter.tsx
+++ front/src/components/atoms/letter/Letter.tsx
@@ -1,14 +1,20 @@
+import convertDateString from "../../../util/date/convertDateString";
 import styles from "./Letter.module.scss";
 
 interface Props {
   sender?: string;
   content: string;
+  onClick?: () => void;
+  item: any;
 }
 
-function Letter({ sender, content }: Props) {
+function Letter({ sender, content, item, onClick }: Props) {
   return (
-    <article className={styles.letter_container}>
+    <article className={styles.letter_container} onClick={onClick}>
       <div className={styles.content_box}>{content}</div>
+      <div className={styles.writeTime}>
+        {convertDateString(item.write_time)}
+      </div>
       <div className={styles.name_box}>From.{sender ? sender : "익명"}</div>
     </article>
   );
