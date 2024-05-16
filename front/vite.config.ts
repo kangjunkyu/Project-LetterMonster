ea232af3d47f4c7127d4c10300164ac6d521bcbb
이호진 <skwo777@naver.com>
2024-05-16 14:49:48 +0900

    Build: 빌드 에러 수정

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -5,12 +5,4 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: "index.html",
-        sw: "firebase-messaging-sw.js", // 서비스 워커 파일 추가
-      },
-    },
-  },
 });
