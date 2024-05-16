c502eee850b7d387f89b545d4d58d7507f79a458
이호진 <skwo777@naver.com>
2024-05-16 12:56:30 +0900

    Refactor: FCM IOS 분기 로직 변경

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -5,4 +5,12 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        sw: "firebase-messaging-sw.js", // 서비스 워커 파일 추가
+      },
+    },
+  },
 });
